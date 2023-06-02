6691c42adbca7464ff52062b1cabd02ae4b0bdf9
unknown <arundvp@gmail.com>
2023-06-01 22:11:42 -0700

    update saveBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-  // Display the current date in the header
   var currentDate = dayjs().format("dddd, MMMM D, YYYY");
-  $("#currentDay").text(currentDate);
+  var currentHour = new Date().getHours();
+  var timeBlock = document.getElementById('hour-9');
+  var timeBlockHour = parseInt(timeBlock.querySelector('.hour').textContent);
 
-  // Add a listener for click events on the save buttons
+  $("#currentDay").text(currentDate);
+  
   $(".save-btn").on("click", function() {
     var timeBlockId = $(this).parent().attr("id");
     var userInput = $(this).siblings(".description").val();
     localStorage.setItem(timeBlockId, userInput);
   });
 
-  // Apply the past, present, or future class to each time block
   $(".time-block").each(function() {
     var timeBlockId = $(this).attr("id");
     var currentHour = dayjs().hour();
     var blockHour = parseInt(timeBlockId.split("-")[1]);
 
     if (blockHour < currentHour) {
-      $(this).removeClass("present future").addClass("past");
-    } else if (blockHour === currentHour) {
-      $(this).removeClass("past future").addClass("present");
+      $(this).addClass("past");
+          } else if (blockHour === currentHour) {
+        $(this).addClass("present");
     } else {
-      $(this).removeClass("past present").addClass("future");
-    }
-  });
+      $(this).addClass("future");
+          }
+    });
 
-  // Get any user input saved in localStorage and set the corresponding textarea values
   $(".time-block").each(function() {
     var timeBlockId = $(this).attr("id");
     var userInput = localStorage.getItem(timeBlockId);
